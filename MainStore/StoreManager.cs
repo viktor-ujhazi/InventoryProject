a0ca2956f635f66ad431a647b7aed1187c88acc1
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-01-28 12:25:46 +0100

    implement addCDProduct and addBookProduct

--- MainStore/StoreManager.cs
+++ MainStore/StoreManager.cs
@@ -6,11 +6,22 @@ namespace MainStore
 {
     class StoreManager
     {
-        public void addStorage(StorageCapable storage) { }
-        public void addCDProduct(string name, int price, int tracks) { }
-        public void addBookProduct(string name, int price, int pages) { }
+        private StorageCapable Storage { get; set; }
+        public void addStorage(StorageCapable storage) 
+        {
+            Storage = storage;
+        }
+        public void addCDProduct(string name, int price, int tracks) 
+        {
+            Storage.storeCDProduct(name, price, tracks);
+        }
+        public void addBookProduct(string name, int price, int pages) 
+        {
+            Storage.storeBookProduct(name, price, pages);
+        }
         public String listProducts() 
         {
+            
             return "";
         }
         public int getTotalProductPrice()
