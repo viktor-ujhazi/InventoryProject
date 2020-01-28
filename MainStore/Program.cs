1ba9fb53b93b95dd45cb4fba652cf5772ef71206
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-01-28 12:19:40 +0100

    basic menu

--- MainStore/Program.cs
+++ MainStore/Program.cs
@@ -7,8 +7,42 @@ namespace MainStore
         static void Main(string[] args)
         {
             StoreManager storeManager = new StoreManager();
-            
+            PersistentStore persistentStore = new PersistentStore();
+            storeManager.addStorage(persistentStore);
+            while (true)
+            {
+                Console.WriteLine("(1) Add product");
+                Console.WriteLine("(2) List products");
+                Console.WriteLine("(0) Exit");
+
+                String userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        string type="";
+                        string name="";
+                        int price=0;
+                        int size=0;
+
+                        persistentStore.storeBookProduct(name,price,size);
+                        storeManager.
+                        break;
+                    case "2":
+                        Console.WriteLine(storeManager.listProducts());
+                        break;
+                    case "0":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid choice.");
+                        break;
+                }
+
+
+            }
 
         }
+        
     }
 }
