454ea6576eca17faa4a9938fc161b50a786a6e71
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-01-28 14:50:07 +0100

    implement a test menu

--- MainStore/Program.cs
+++ MainStore/Program.cs
@@ -20,13 +20,45 @@ namespace MainStore
                 switch (userInput)
                 {
                     case "1":
-                        string type="";
-                        string name="";
+                        Console.Write("Enter the type of the product (CD/Book): ");
+                        string type=Console.ReadLine();
+                        Console.Write("Enter the name of the product: ");
+                        string name=Console.ReadLine();
+                        Console.Write("Enter the price of the product: ");
+
                         int price=0;
+                        int.TryParse(Console.ReadLine(), out price);
+
                         int size=0;
 
-                        persistentStore.storeBookProduct(name,price,size);
-                        storeManager.
+                        if (type.ToUpper().Equals("CD"))
+                        {
+                            Console.WriteLine("Enter the number of tracks: ");
+                            int.TryParse(Console.ReadLine(), out size);
+                        }
+                        else if (type.ToUpper().Equals("BOOK"))
+                        {
+                            Console.Write("Enter the number of pages: ");
+                            int.TryParse(Console.ReadLine(), out size);
+                        }
+                        else
+                        {
+                            Console.WriteLine("invalid input.");
+                        }
+
+                        if (type.ToUpper().Equals("CD"))
+                        {
+                            storeManager.addCDProduct(name, price, size);
+                        }
+                        else if (type.ToUpper().Equals("BOOK"))
+                        {
+                            storeManager.addBookProduct(name, price, size);
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                        
                         break;
                     case "2":
                         Console.WriteLine(storeManager.listProducts());
